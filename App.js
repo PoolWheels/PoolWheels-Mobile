91a40dabc55aeda01c174bbdb51b9c822dcd41dc
Camilo Pichimata <camilo.pichimata@mail.escuelaing.edu.co>
2022-11-29 17:14:46 -0500

    Vista login y navegación de la app

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Login from "./components/Login";
+// import NewTripView from "./components/NewTripView";
+// import HomeTravelerUser from "./components/HomeTravelerUser";
+// import NewpaymethosView from "./components/NewpaymethosView";
+// import Paymethodshome from "./components/Paymethodshome";
+// import Soporte from "./components/Soporte";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack.Screen name="Soporte" component={Soporte} />
+        <Stack.Screen name="PayMethods" component={Paymethodshome} />
+        <Stack.Screen name="NewPayMethods" component={NewpaymethosView} />
+        <Stack.Screen name="HomeTravelerUser" component={HomeTravelerUser} />
+        <Stack.Screen name="NewTripView" component={NewTripView} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Login.js
@@ -0,0 +1,68 @@
+import { StatusBar } from "expo-status-bar";
+import { Button } from "react-native";
+import { StyleSheet, Text, View, TextInput, Image } from "react-native";
+import { useState } from "react";
+
+import logo from "../assets/logo.png";
+
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const redirect = () => {
+    navigation.navigate("Ejemplo");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+
+      <Text style={styles.textoBienvenido}>Bienvenido</Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        placeholder="Correo Electrónico"
+        autocomplete="email"
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        placeholder="Contraseña"
+      />
+
+      <StatusBar style="auto" />
+
+      <Button onPress={redirect} title="Login" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 250,
+  },
+
+  logo: {
+    height: 250,
+    resizeMode: "contain",
+  },
+
+  textoBienvenido: {
+    fontSize: 30,
+  },
+});
+
+export default Login;
