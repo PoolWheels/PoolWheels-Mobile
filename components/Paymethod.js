f8d1ac7a85f437b40e0cd9d5c51e632bc5dbc920
eduardoospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-11-29 16:37:49 -0500

    adding necesary style.js for the estructure of styles.

--- components/Paymethod.js
+++ components/Paymethod.js
@@ -1,3 +1,4 @@
 import { React, useState} from "react";
 import { Alert, Modal, View, Text, Pressable } from "react-native";
 import styles from "../styles/trip";
+import stylesModal from "../styles/modalstyle";
