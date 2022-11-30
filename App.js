ed242418a07ce22df94bc281b05c3771c7639077
Nataorjuela <nataorjuela62@gmail.com>
2022-11-30 13:04:56 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import NewpaymethosView from "./components/NewPayMethods";
+import Paymethodshome from "./components/Paymethodshome";
+
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Metodos de Pago" component={Paymethodshome} />
+        <Stack.Screen name="Agregar Metodo de Pago" component={NewpaymethosView} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Paymethodshome.js
@@ -0,0 +1,74 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View, Dimensions, FlatList, Animated, Alert, Pressable} from "react-native";
+import styles  from "../styles/Paymethod-style.js"
+import { paymethodsuser} from "../utils/dataPM";
+import { React, useState, useRef} from "react";
+import Paymethod from "./Paymethod.js";
+
+export default function Paymethodshome(props) {
+
+    const [paymeths, setpaymeths] = useState(paymethodsuser);
+    const width = Dimensions.get("screen").width;
+
+    const deletepaymethod = async(idpaymeth) => {
+        Alert.alert(" Eliminado ", " se elimino el metodo de pago.");
+        setpaymeths(await deleteTrip(idpaymeth));
+      };
+
+    return (
+        <View style={styles.container}>
+        <StatusBar style="light" />
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <View>
+          <Text style={styles.title}>🪙 Estos son tus metodos de pago 🏦</Text>
+        </View>
+
+
+        <Text>
+        </Text>
+        <Pressable style={styles.button} 
+        onPress={() => props.navigation.navigate("Agregar Metodo de Pago")} >
+            <Text style={styles.text}>    💳 Agregar Nuevo metodo de pago   </Text>
+        </Pressable>
+        <Text>
+        </Text>
+
+        <View style={{  height: '85%'}}>
+            <FlatList
+            data={paymeths}
+            keyExtractor={(_, index) => index}
+            horizontal={true}
+            pagingEnabled
+            showsHorizontalScrollIndicator={true}
+            renderItem={({ item, index }) => {
+                return (
+                <View style={{ width, ustifyContent: "center", alignItems: "center"}}>
+                    <Paymethod
+                    id={item.id}
+                    owner={item.owner}
+                    number={item.number}
+                    type={item.type}
+                    bank={item.bank}
+                    expirationDate={item.expirationDate}
+                    index={index}
+                    contentButton={"ELIMINAR"}
+                    functionOne={deletepaymethod}
+                    ></Paymethod>
+                </View>
+                );
+            }}
+            >
+
+            </FlatList>
+            <Text></Text>
+
+            
+        </View>
+      </View>
+    )
+      
+};
