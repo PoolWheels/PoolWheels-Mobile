7e354748b5a2a871416f4371a10c98dd3439c684
Jose Gamboa <jose.gamboa@mail.escuelaing.edu.co>
2022-11-29 20:41:25 -0500

    Terminación funcionalidades principales sin estilos

---
+++ components/HomeUserDriver.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Constants from 'expo-constants'
+import { View, Text, FlatList, Button, Alert } from 'react-native'
+import {trips} from '../utils/dataTrips'
+
+export default function HomeUserDriver(props) {
+    
+    const formatChildItems = (stops) => {
+        const arrayStops = [];
+
+        for (let key in stops) {
+        arrayStops.push([key.replace(/_+/g, ' '), stops[key]]);
+        }
+        return arrayStops;
+    };
+    
+    return (
+        <View>
+            <FlatList
+                data={trips}
+                renderItem={({item : trip}) => (
+                    <View>
+                        <Text>Punto de partida: {trip.addrInit}</Text>
+                        <Text>Dirección de Destino: {trip.addrFin}</Text>
+                        <Text>Puestos Disponibles: {trip.availableSeats}</Text>
+                        <Text>Paradas:</Text>
+                        {formatChildItems(trip.stops).map((stop) => (
+                            <Text>
+                                {"          "}
+                                {stop[0] + ": " + stop[1]}
+                            </Text>
+                        ))}
+                        <Text>Pasajeros:</Text>
+                        {formatChildItems(trip.passengers).map((passengers) => (
+                            <Text>
+                                {"          "}
+                                {passengers[0] + ": " + passengers[1]}
+                            </Text>
+                        ))}
+                        <Text>Hora de salida: {trip.finTime}</Text>
+                    </View>
+                )}
+            />
+            <Button
+                title='Crear nuevo viaje'
+                onPress={() => props.navigation.navigate("NewTripForm")}
+            />
+        </View>
+        
+    )
+}
