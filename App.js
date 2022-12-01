2437038b954380993bd2ab9ff87edef629c12697
Nataorjuela <nataorjuela62@gmail.com>
2022-11-30 19:14:47 -0500

    correction

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import NewpaymethosView from "./components/NewPayMethods";
+import Login from "./components/Login";
+import NewTripView from "./components/NewTripView";
+import HomeTravelerUser from "./components/HomeTravelerUser";
+import NewpaymethosView from "./components/NewpaymethosView";
 import Paymethodshome from "./components/Paymethodshome";
-
+import Soporte from "./components/Soporte";
+import HomeUserDriver from "./components/HomeUserDriver";
+import NewTrip from "./components/NewTrip";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -11,10 +16,15 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Metodos de Pago" component={Paymethodshome} />
-        <Stack.Screen name="Agregar Metodo de Pago" component={NewpaymethosView} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Soporte" component={Soporte} />
+        <Stack.Screen name="PayMethods" component={Paymethodshome} />
+        <Stack.Screen name="NewPayMethods" component={NewpaymethosView} />
+        <Stack.Screen name="HomeTravelerUser" component={HomeTravelerUser} />
+        <Stack.Screen name="NewTripView" component={NewTripView} />
+        <Stack.Screen name="HomeDriverUser" component={HomeUserDriver} />
+        <Stack.Screen name="NewTripForm" component={NewTrip} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
---
+++ components/Login.js
@@ -0,0 +1,93 @@
+import { StatusBar } from "expo-status-bar";
+import { Button } from "react-native";
+import { StyleSheet, Text, View, TextInput, Image, Switch } from "react-native";
+import { useState } from "react";
+
+import logo from "../assets/logo.png";
+
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
+  const redirect = () => {
+    if (isEnabled) {
+      navigation.navigate("HomeUserDriver");
+    } else {
+      navigation.navigate("HomeTravelerUser");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        placeholder="Correo Electrónico"
+        autocomplete="email"
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        placeholder="Contraseña"
+      />
+
+      <StatusBar style="auto" />
+
+      <View style={styles.container_dos}>
+        <Text>Usuario Conductor</Text>
+
+        <Switch
+          trackColor={{ false: "#767577", true: "#3E3BC7" }}
+          thumbColor={isEnabled ? "#8C8ADE" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </View>
+
+      <Button onPress={redirect} title="Login" color="#3E3BC7" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  container_dos: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 20,
+  },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 250,
+  },
+
+  logo: {
+    height: 170,
+    resizeMode: "contain",
+    marginBottom: 30,
+  },
+
+  textoBienvenido: {
+    fontSize: 30,
+  },
+});
+
+export default Login;
