8701e1bfa085664db8447e60b419c4c8728550a7
Kristhian David Segura <kristhian.segura@mail.escuelaing.edu.co>
2022-11-30 22:13:36 -0500

    Correccion app.js

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-// import Login from "./components/Login";
-// import NewTripView from "./components/NewTripView";
-// import HomeTravelerUser from "./components/HomeTravelerUser";
-// import NewpaymethosView from "./components/NewpaymethosView";
-// import Paymethodshome from "./components/Paymethodshome";
+import Login from "./components/Login";
+import NewTripView from "./components/NewTripView";
+import HomeTravelerUser from "./components/HomeTravelerUser";
+import NewpaymethosView from "./components/NewpaymethosView";
+import Paymethodshome from "./components/Paymethodshome";
 import Soporte from "./components/Soporte";
-// import HomeUserDriver from "./components/HomeUserDriver";
-// import NewTrip from "./components/NewTrip";
+import HomeUserDriver from "./components/HomeUserDriver";
+import NewTrip from "./components/NewTrip";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -16,14 +16,14 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* <Stack.Screen name="Login" component={Login} /> */}
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Soporte" component={Soporte} />
-        {/* <Stack.Screen name="PayMethods" component={Paymethodshome} />
+        <Stack.Screen name="PayMethods" component={Paymethodshome} />
         <Stack.Screen name="NewPayMethods" component={NewpaymethosView} />
         <Stack.Screen name="HomeTravelerUser" component={HomeTravelerUser} />
         <Stack.Screen name="NewTripView" component={NewTripView} />
         <Stack.Screen name="HomeDriverUser" component={HomeUserDriver} />
-        <Stack.Screen name="NewTripForm" component={NewTrip} /> */}
+        <Stack.Screen name="NewTripForm" component={NewTrip} />
       </Stack.Navigator>
     </NavigationContainer>
   );
