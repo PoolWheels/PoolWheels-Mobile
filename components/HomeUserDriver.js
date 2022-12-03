26c06316e27e1d0e62c5c3c2e86e0c898ea8ef44
Jose Gamboa <jose.gamboa@mail.escuelaing.edu.co>
2022-12-02 19:25:45 -0500

    Arreglo botones Vistas Conductor

--- components/HomeUserDriver.js
+++ components/HomeUserDriver.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text, FlatList, Button, Dimensions } from 'react-native'
 import {trips} from '../utils/dataTrips'
 import styles from "../styles/hometrav-user";
+import styles2 from "../styles/trip"
 
 export default function HomeUserDriver(props) {
     
@@ -16,56 +17,66 @@ export default function HomeUserDriver(props) {
     
     return (
       <View style={styles.container}>
-        <FlatList
-          data={trips}
-          keyExtractor={(_, index) => index}
-          horizontal={true}
-          pagingEnabled
-          showsHorizontalScrollIndicator={true}
-          renderItem={({ item: trip }) => (
-            <View
-              style={{
-                width: Dimensions.get("screen").width,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Text>Punto de partida: {trip.addrInit}</Text>
-              <Text>Dirección de Destino: {trip.addrFin}</Text>
-              <Text>Puestos Disponibles: {trip.availableSeats}</Text>
-              <Text>Paradas:</Text>
-              {formatChildItems(trip.stops).map((stop) => (
-                <Text>
-                  {"          "}
-                  {stop[0] + ": " + stop[1]}
-                </Text>
-              ))}
-              <Text>Pasajeros:</Text>
-              {formatChildItems(trip.passengers).map((passengers) => (
-                <Text>
-                  {"          "}
-                  {passengers[0] + ": " + passengers[1]}
-                </Text>
-              ))}
-              <Text>Hora de salida: {trip.finTime}</Text>
-            </View>
-          )}
-        />
+        <View>
+          <Text style={styles.title}>Bienvenida Lorena 👋!</Text>
+        </View>
+        <View style={{ flex:1 }}>
+          <FlatList
+            data={trips}
+            keyExtractor={(_, index) => index}
+            horizontal={true}
+            pagingEnabled
+            extraData={trips}
+            showsHorizontalScrollIndicator={true}
+            renderItem={({ item: trip }) => (
+              <View
+                style={{
+                  width: Dimensions.get("screen").width,
+                  justifyContent: "center",
+                  alignContent: "center",
+                  alignItems: 'center',
+                  resizeMode: "cover",
+                  borderRadius: 5,
+                  marginBottom: 20,
+                  borderWidth: 1.5,
+                  borderColor: "rgb(27,74,255)",
+                }}
+              >
+                <Text style={styles2.subtitles}>Punto de partida: {trip.addrInit}</Text>
+                <Text style={styles2.subtitles}>Dirección de Destino: {trip.addrFin}</Text>
+                <Text style={styles2.subtitles}>Puestos Disponibles: {trip.availableSeats}</Text>
+                <Text style={styles2.subtitles}>Paradas:</Text>
+                {formatChildItems(trip.stops).map((stop) => (
+                  <Text style={styles2.hour}>
+                    {"          "}
+                    {stop[0] + ": " + stop[1]}
+                  </Text>
+                ))}
+                {/* <Text>Pasajeros:</Text>
+                {formatChildItems(trip.passengers).map((passengers) => (
+                  <Text>
+                    {"          "}
+                    {passengers[0] + ": " + passengers[1]}
+                  </Text>
+                ))} */}
+                <Text style={styles2.hour}>Hora de salida: {trip.finTime}</Text>
+              </View>
+            )}
+          />
+        </View>
         <Button
-          style={styles.buttonOption}
           title="Crear nuevo viaje"
           onPress={() => props.navigation.navigate("NewTripForm")}
         />
-        <View style={{ flexDirection: "row" }}>
+        <View style={{ flexDirection: "row", margin: 10 }}>
           <Button
-            style={styles.buttonOption}
             title="Metodos de pago"
             onPress={() => {
               props.navigation.navigate("PayMethods");
             }}
           />
+          <Text>  </Text>
           <Button
-            style={styles.buttonOption}
             title="Soporte"
             onPress={() => {
               props.navigation.navigate("Soporte");
